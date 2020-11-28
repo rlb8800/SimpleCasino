a885d699e8ac2447eb04d6315ed9b647e35631a6
Ryan Borger <rlb8800@g.rit.edu>
2020-11-28 11:18:37 -0500

    message commit

--- src/main/java/simplecasino/simplecasino/SimpleCasino.java
+++ src/main/java/simplecasino/simplecasino/SimpleCasino.java
@@ -7,6 +7,7 @@ public final class SimpleCasino extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Yay the plugin works");
 
     }
 

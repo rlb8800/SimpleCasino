71270d20e3c651533f70f05373e090574fd4d30b
Ryan Borger <rlb8800@g.rit.edu>
2020-11-28 12:23:27 -0500

    Added Vault

--- src/main/java/simplecasino/simplecasino/SimpleCasino.java
+++ src/main/java/simplecasino/simplecasino/SimpleCasino.java
@@ -1,14 +1,22 @@
 package simplecasino.simplecasino;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimpleCasino extends JavaPlugin {
 
+    private static Economy econ = null;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Yay the plugin works");
-        //works?
+
+        if (!setupEconomy() ) {
+            System.out.println("No Economy plugin detected");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
     }
 
@@ -16,4 +24,23 @@ public final class SimpleCasino extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+
 }

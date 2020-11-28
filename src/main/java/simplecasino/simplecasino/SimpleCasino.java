a004e66e30f87b86c0853ea2642b10d1e90beb1a
Ryan Borger <rlb8800@g.rit.edu>
2020-11-28 12:05:06 -0500

    removed crap

--- src/main/java/simplecasino/simplecasino/SimpleCasino.java
+++ src/main/java/simplecasino/simplecasino/SimpleCasino.java
@@ -7,8 +7,6 @@ public final class SimpleCasino extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Yay the plugin works");
-        //works?
 
     }
 

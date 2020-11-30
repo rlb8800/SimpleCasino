8e5f47de83c3a8d22946e5305615948fcca8225a
Ryan Borger <47132584+rlb8800@users.noreply.github.com>
2020-11-30 16:43:41 -0500

    Args perameters
    added handling for args

--- src/main/java/Commands/openGUI.java
+++ src/main/java/Commands/openGUI.java
@@ -20,26 +20,33 @@ public class openGUI implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player){
             Player p = (Player) sender;
-            Inventory inv = Bukkit.createInventory(null, 45, "Gambling Machine");
-            ItemStack redpane = new ItemStack(Material.RED_STAINED_GLASS_PANE);
-            ItemStack greenpane = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
-            ItemMeta meta1 = redpane.getItemMeta();
-            ItemMeta meta2 = greenpane.getItemMeta();
-            meta1.setDisplayName((ChatColor.RED + "Blunder"));
-            meta2.setDisplayName((ChatColor.GREEN + "Cash Out"));
-            redpane.setItemMeta(meta1);
-            greenpane.setItemMeta(meta2);
-            inv.setItem(18, redpane);
-            inv.setItem(20, redpane);
-            inv.setItem(22, redpane);
-            inv.setItem(24, redpane);
-            inv.setItem(26, redpane);
-            inv.setItem(19, greenpane);
-            inv.setItem(21, greenpane);
-            inv.setItem(23, greenpane);
-            inv.setItem(25, greenpane);
-            p.openInventory(inv);
-            return true;
+            if(args.length != 2) {
+                //TODO
+            }
+            if(args.length == 2) {
+                int betAmt = args[1];
+                Inventory inv = Bukkit.createInventory(null, 45, "Gambling Machine");
+                ItemStack redpane = new ItemStack(Material.RED_STAINED_GLASS_PANE);
+                ItemStack greenpane = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
+                ItemMeta meta1 = redpane.getItemMeta();
+                ItemMeta meta2 = greenpane.getItemMeta();
+                meta1.setDisplayName((ChatColor.RED + "Blunder"));
+                meta2.setDisplayName((ChatColor.GREEN + "Cash Out"));
+                redpane.setItemMeta(meta1);
+                greenpane.setItemMeta(meta2);
+                inv.setItem(18, redpane);
+                inv.setItem(20, redpane);
+                inv.setItem(22, redpane);
+                inv.setItem(24, redpane);
+                inv.setItem(26, redpane);
+                inv.setItem(19, greenpane);
+                inv.setItem(21, greenpane);
+                inv.setItem(23, greenpane);
+                inv.setItem(25, greenpane);
+                p.openInventory(inv);
+                return true;
+            }
+      
         }
         return false;
     }
